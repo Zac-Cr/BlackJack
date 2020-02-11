4b77b242f2763bd967cb8097acd774a1aa87fe9d
the1stof300 <zcrock300@gmail.com>
2020-02-10 20:48:54 -0700

    dealer

--- blackjack.js
+++ blackjack.js
@@ -12,7 +12,7 @@ const deck_of_cards ={
     "Jack" : 10,
     "Queen" : 10,
     "King" : 10,
-    "Ace" : [1,11]
+    "Ace" : 11,
 };
 const deck_names=[2,3,4,5,6,7,8,9,10,"Jack","Queen","King","Ace"];
 const player_hand_values = [];
@@ -22,7 +22,7 @@ const dealer_hand_name = [];
 const prompt =require('prompt-sync')();
 let game = "Running"
 let money = 100;
-let wager = 0
+let wager = 0;
 
 function set_the_game(){
     console.log("You have made it to the World Championship BlackJack game.\nThe competition has narrowed down to just you and the dealer.\n Do you have what it takes to bring it all home?")
@@ -35,8 +35,9 @@ function set_the_game(){
     dealer_hand_values.push(deck_of_cards[dealer_hand_name[0]]);    
     console.log(`The dealer has a ${dealer_hand_name[0]}`);   
 }
-funciton get_bet(wage){
+function get_bet(){
     wager = ("what would you like to bet","");
+    return wager;
 }
 function get_action(){
     const input = prompt('what action would you like to do\nstand\nhit\ndoubledown\nsplit the pair\n', "");
@@ -49,7 +50,6 @@ function decide_action(){
         let stat = get_action();
         if (stat === "stand"){
             // do nothing let the dealer go
-            check_for_bust();
         };
         if (stat === "hit"){
             draw_card(player_hand_name, player_hand_values, index)
@@ -60,11 +60,17 @@ function decide_action(){
             // double the bet value and add another card and stop player from building anymore
             check_for_bust();
         };
-
     };
     
 }
 
+function dealer_action(){
+    draw_card(player_hand_name,dealer_hand_values, index);
+    let dealer_total = dealer_hand_values.reduce(function(x,y){return x + y});
+
+    
+
+}
 function draw_card(person,person_values, index ){
     let card = deck_names[Math.floor(Math.random() * deck_names.length)];
     person.push(card);
@@ -100,7 +106,7 @@ function check_for_bust(hand){
     if(total > 21){
         console.log(`Shoot, looks like you busted`);
         game = "Lost";
-        wallet();
+        wallet(money, wager);
         // Run dealer cards
     }
 
