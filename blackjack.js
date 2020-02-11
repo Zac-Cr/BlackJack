444e6d5166ea683d11fb14ecbf0f513eb1fb0df3
the1stof300 <zcrock300@gmail.com>
2020-02-11 13:10:14 -0700

    running game without double Ace values

--- blackjack.js
+++ blackjack.js
@@ -15,87 +15,99 @@ const deck_of_cards ={
     "Ace" : 11,
 };
 const deck_names=[2,3,4,5,6,7,8,9,10,"Jack","Queen","King","Ace"];
-const player_hand_values = [];
-const player_hand_name = [];
-const dealer_hand_values = [];
-const dealer_hand_name = [];
-const prompt =require('prompt-sync')();
+let player_hand_values = [];
+let player_hand_name = [];
+let dealer_hand_values = [];
+let dealer_hand_name = [];
+const prompt = require('prompt-sync')();
 let game = "Running"
 let money = 100;
 let wager = 0;
 let index = 2;
 
 function set_the_game(){
-    console.log("You have made it to the World Championship BlackJack game.\nThe competition has narrowed down to just you and the dealer.\n Do you have what it takes to bring it all home?")
+    player_hand_name = [];
+    player_hand_values = [];
+    dealer_hand_values = [];
+    dealer_hand_name= [];
     player_hand_name.push(deck_names[Math.floor(Math.random() * deck_names.length)]);
     player_hand_name.push(deck_names[Math.floor(Math.random() * deck_names.length)]);
     player_hand_values.push(deck_of_cards[player_hand_name[0]]);
     player_hand_values.push(deck_of_cards[player_hand_name[1]]);
-    console.log(`You have been dealt  ${player_hand_name[0]} and ${player_hand_name[1]}`)
+    console.log(`You have been dealt ${player_hand_name[0]} and ${player_hand_name[1]}`)
     dealer_hand_name.push(deck_names[Math.floor(Math.random() * deck_names.length)]);
     dealer_hand_values.push(deck_of_cards[dealer_hand_name[0]]);    
     console.log(`The dealer has a ${dealer_hand_name[0]}`);   
 }
 function get_bet(){
-    wager = prompt(`what would you like to bet\nYou have $${money}\n`,"");
+    console.log(`What would you like to bet\nYou have $${money}`);
+    wager = prompt("","");
     return wager;
 }
 function get_action(){
-    const input = prompt('what action would you like to do\nstand\nhit\ndoubledown\nsplit the pair\n', " ");
+    console.log('what action would you like to do\nstand\nhit\ndouble down\nsplit the pair');
+    const input = prompt("", "");
     return input;
 }
 
 function decide_action(){
-    let stat = get_action();
     while(game === "Running"){
-        
+        let stat = get_action();
         if (stat === "stand"){
             dealer_action();
             decide_game();
         };
         if (stat === "hit"){
-            draw_card(player_hand_name, player_hand_values, index)
-            console.log(`you have been dealt a ${player_hand_name[index]}`);
-            console.log(`you now have a ${player_hand_name[0]} a ${player_hand_name[1]} and a ${player_hand_name[2]}`);
+            draw_card(player_hand_name, player_hand_values)
+            console.log("You have been dealt");
+            for ( item of player_hand_name){
+                console.log(`a ${item},`);
+            };
             check_for_bust(player_hand_values);
-            index ++
         };
         if (stat === "double down"){
-            draw_card(player_hand_name,player_hand_values,index);
+            draw_card(player_hand_name,player_hand_values,);
+            console.log("You have been dealt");
+            for ( item of player_hand_name){
+                console.log(`a ${item},`);
+            };
             wager = wager * 2;
             check_for_bust(player_hand_values);
-            dealer_action();
-            decide_game();
         };
-    };    
+    };  
+    wallet(money, wager);  
 }
 function decide_game(){
     let player_hand = player_hand_values.reduce(function(x,y){return x + y});
     let dealer_hand = dealer_hand_values.reduce(function(x,y){return x + y});
+    console.log("The game is decided");
     if(player_hand > dealer_hand){
         game = "Won";
+        console.log("You have won");
     }
     else{
-        game = "Loss"
+        game = "Lost"
+        console.log("You have lost");
     };
+ 
 }
 function dealer_action(){
-    draw_card(dealer_hand_name,dealer_hand_values, index);
+    draw_card(dealer_hand_name,dealer_hand_values);
     let dealer_total = dealer_hand_values.reduce(function(x,y){return x + y});
-    if(dealer_total < 10){
-        draw_card(player_hand_name,dealer_hand_values, index);
+    console.log(`It looks like the second card for the dealer is a ${dealer_hand_name[1]}`)
+    if(dealer_total <= 10){
+        draw_card(dealer_hand_name,dealer_hand_values);
         dealer_total = dealer_hand_values.reduce(function(x,y){return x + y});
         console.log(`The third card for the dealer is ${dealer_hand_name[2]}`)
     };
-    console.log(`It looks like the second card for the dealer is a ${dealer_hand_name[1]}`)
+   
 }
-function draw_card(person,person_values, index ){
-    let card = deck_names[Math.floor(Math.random() * deck_names.length)];
+function draw_card(person,person_values){
+    var card = deck_names[Math.floor(Math.random() * deck_names.length)];
     person.push(card);
-    person_values.push(deck_of_cards[person[index]]);
-    if(check_cards(person,card)){
-        draw_card(person,person_values,index);
-    };
+    let x = person.pop();
+    person.push(x);
+    person_values.push(deck_of_cards[x]);
 }
 function check_cards(list, item){
     let count = 0;
@@ -109,15 +121,15 @@ function check_cards(list, item){
     }
 }
 function wallet(amount, bet){
-    if (game === "Won"){
+    if (game == "Won"){
         money = parseInt(amount) + parseInt(bet);
         console.log(`You won $${bet}`);
     };
-    if (game ==="Lost"){
+    if (game =="Lost"){
         money = amount - bet;
         console.log(`You lost $${bet}`);
-    }
-    console.log(`You now have $${money}`)
+    };
+    console.log(`You now have $${money}`);
     return money;
 }
 function check_for_bust(hand){
@@ -125,11 +137,26 @@ function check_for_bust(hand){
     if(total > 21){
         console.log(`Shoot, looks like you busted`);
         game = "Lost";
-        return true
-        // Run dealer cards
+        return true        
     }
 
 }
-set_the_game();
-get_bet();
-decide_action();
+function continue_game(){
+    console.log("Would you like to continue?\nYes or No");
+    let answer = prompt("","");
+    answer = answer.toLowerCase();
+    if(answer === "no"){
+        desire = "no";
+    }
+}
+
+// game running code
+let desire = "Continue";
+console.log("You have made it to the World Championship BlackJack game.\nThe competition has narrowed down to just you and the dealer.\n Do you have what it takes to bring it all home?");
+while (desire ==="Continue"){
+    game = "Running";
+    set_the_game();
+    get_bet();
+    decide_action();
+    continue_game();
+}
